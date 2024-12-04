0ad8effd94b95a7300929855233fe3bbb662e736
Samrat Khan <takeitkhan@gmail.com>
2024-12-04 15:18:28 +0600

    some changes

--- components/Services.js
+++ components/Services.js
@@ -1,30 +1,24 @@
 // "use client";
 
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image'
+import Link from 'next/link'
 
 // ==== icons ====
-import logo1 from "../public/image/icons/cellular-tower.png";
-import logo2 from "../public/image/icons/Satellite.png";
-import logo3 from "../public/image/icons/ip.png";
-import logo4 from "../public/image/icons/data center.png";
-import logo5 from "../public/image/icons/wireless.png";
-import logo6 from "../public/image/icons/e.png";
-import { fetchClient } from "@/helpers/fetchClient";
+import logo1 from '../public/image/icons/cellular-tower.png'
+import { fetchClient } from '@/helpers/fetchClient'
+import { getMetaValueByMetaName } from '@/helpers/metaHelpers'
 
 const Services = async () => {
   let services;
 
   try {
-    const url = `/posts?term_type=services`;
+    const url = `/posts?term_type=services`
     const responseData = await fetchClient(url, {
       next: {
-        revalidate: 30,
-      },
-    });
-
-    // console.log(responseData);
-    services = responseData?.data;
+        revalidate: 30
+      }
+    })    
+    services = responseData?.data    
   } catch (err) {
     // return (
     //   <div className="text-center text-red-600">
@@ -39,38 +33,44 @@ const Services = async () => {
 
   return (
     <>
-      <section className="relative">
+      <section className='relative'>
         <video
-          src="https://video.wixstatic.com/video/f21e62_ded834d5089c4be39db624ab8cdc304d/1080p/mp4/file.mp4"
+          src='https://video.wixstatic.com/video/f21e62_ded834d5089c4be39db624ab8cdc304d/1080p/mp4/file.mp4'
           autoPlay
           loop
           muted
-          className="absolute left-0 top-0 -z-10 object-cover w-full h-full"
+          className='absolute left-0 top-0 -z-10 object-cover w-full h-full'
         ></video>
-        <div className="container mx-auto py-20">
-          <div className="md:grid md:grid-cols-4 md:gap-10 gap-7 grid">
-            {services?.map((service, index) => (
+        <div className='absolute inset-0 bg-black/80'></div>{' '}
+        {/* Black overlay with 0.4 opacity */}
+        <div className='relative container mx-auto py-20'>
+          {/* { JSON.stringify(services) } */}
+          {' '}          
+          <div className='md:grid md:grid-cols-4 md:gap-10 gap-7 grid'>            
+            {services?.map((service, index) => (              
               <div
                 key={index}
-                className="backdrop-blur-md p-3 text-white rounded-md md:mt-20"
-              >
+                className='text-white min-h-[450px] flex flex-col group w-48'
+              >                
                 <Image
-                  src={logo1}
+                  src={getMetaValueByMetaName(services, 'service_icon')}
                   width={35}
                   height={35}
                   alt={`${service?.name} logo`}
                 />
-                <h1 className="text-2xl font-bold my-2">{service?.name}</h1>
-                <p>
-                  {service?.description
-                    ? `${service.description.slice(0, 100)}${
-                        service.description.length > 100 ? "..." : ""
-                      }`
-                    : ""}
-                </p>
+                <h1 className='text-2xl font-bold my-2'>{service?.name}</h1>                
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: service?.description
+                      ? `${service.description.slice(0, 200)}${
+                          service.description.length > 200 ? '...' : ''
+                        }`
+                      : ''
+                  }}
+                ></p>
                 <Link
                   href={`services/${service?.slug}`}
-                  className="underline mt-5 block font-semibold capitalize"
+                  className='underline mt-5 block font-semibold capitalize'
                 >
                   Learn more
                 </Link>
@@ -80,7 +80,7 @@ const Services = async () => {
         </div>
       </section>
     </>
-  );
-};
+  )
+}
 
-export default Services;
+export default Services
--- helpers/metaHelpers.js
+++ helpers/metaHelpers.js
@@ -1,9 +1,11 @@
 // metaHelpers.js
 
-export const getMetaValueByMetaName = (jsonData, metaName) => {
-  console.log("from helper", jsonData?.data);
-  console.log(metaName);
+export default getMetaValueByMetaName = (jsonData, metaName) => {
+  //console.log("from helper", jsonData?.data);
+  //console.log(metaName);
   const foundItem = jsonData?.data?.find((item) => item.meta_name === metaName);
-  console.log(foundItem);
+  //console.log(foundItem);
   return foundItem ? foundItem.meta_value : null;
 };
+
+
